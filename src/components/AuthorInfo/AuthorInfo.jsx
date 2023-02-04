1279d3eb24e2ed45efe29957b1963b608790c065
Chris Campbell <christopherecampbell@gmail.com>
2023-02-04 11:25:31 -0700

    stub up author info

---
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -0,0 +1,11 @@
+import styles from './AuthorInfo.module.css'
+
+const AuthorInfo = () => {
+  return (  
+    <div>
+      <p>this is AuthorInfo</p>
+    </div>
+  )
+}
+
+export default AuthorInfo;
--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,5 +1,6 @@
 import styles from './PostCard.module.css'
 import Icon from '../Icon/Icon';
+import AuthorInfo from '../AuthorInfo/AuthorInfo';
 
 const PostCard = () => {
   let category = "meal"
@@ -9,6 +10,7 @@ const PostCard = () => {
     <Icon category={category}/>
     <img src="https://picsum.photos/200" alt="post photo" />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et, tempore, doloremque minima maxime nobis reiciendis assumenda suscipit voluptatum ducimus praesentium molestias! Iusto voluptate aspernatur, dolore ratione hic ea provident!</p>
+    <AuthorInfo />
   </div>
   )
 }
