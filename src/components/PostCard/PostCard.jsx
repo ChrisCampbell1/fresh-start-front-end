ed121211c3e1c2037b063269d227d3e7c3634b73
Chris Campbell <christopherecampbell@gmail.com>
2023-02-04 15:25:42 -0700

    adjust css on PostCard

--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -8,8 +8,10 @@ const PostCard = () => {
   return (  
   <div className={styles.container}>
     <h2>Post Title</h2>
-    <Icon category={category}/>
-    <img src="https://picsum.photos/200" alt="post photo" />
+    <div className={styles.icon}>
+      <Icon category={category}/>
+    </div>
+    <img className={styles.postPhoto} src="https://picsum.photos/200" alt="post photo" />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et, tempore, doloremque minima maxime nobis reiciendis assumenda suscipit voluptatum ducimus praesentium molestias! Iusto voluptate aspernatur, dolore ratione hic ea provident!</p>
     <div className={styles.postStats}>
       <AuthorInfo />
