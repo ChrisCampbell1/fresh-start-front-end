7b4acfac75925fc879f9bfe888f043cf2d8556bf
Chris Campbell <christopherecampbell@gmail.com>
2023-02-04 11:18:07 -0700

    add Icon component and icon svg files to assets

---
+++ src/components/Icon/Icon.jsx
@@ -0,0 +1,16 @@
+import barbell from '../../assets/icons/barbell.svg'
+import meal from '../../assets/icons/meal.svg'
+import thought from '../../assets/icons/thought.svg'
+
+const Icon = ({ category }) => {
+  const icons = {
+    barbell,
+    meal,
+    thought
+  }
+  return (  
+    <img className='icon' src={icons[category]} alt={`a ${category} icon`} />
+  )
+}
+
+export default Icon;
--- src/components/PostCard/PostCard.jsx
+++ src/components/PostCard/PostCard.jsx
@@ -1,9 +1,12 @@
 import styles from './PostCard.module.css'
+import Icon from '../Icon/Icon';
 
 const PostCard = () => {
+  let category = "meal"
   return (  
   <div className={styles.container}>
     <h2>Post Title</h2>
+    <Icon category={category}/>
     <img src="https://picsum.photos/200" alt="post photo" />
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt et, tempore, doloremque minima maxime nobis reiciendis assumenda suscipit voluptatum ducimus praesentium molestias! Iusto voluptate aspernatur, dolore ratione hic ea provident!</p>
   </div>
