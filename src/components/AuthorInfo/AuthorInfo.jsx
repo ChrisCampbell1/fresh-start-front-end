fdd75303353991de4cda18f8be0d6d111d652bd3
Chris Campbell <christopherecampbell@gmail.com>
2023-02-04 11:27:47 -0700

    stub up DateCard

--- src/components/AuthorInfo/AuthorInfo.jsx
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -1,9 +1,11 @@
 import styles from './AuthorInfo.module.css'
+import DateCard from '../DateCard/DateCard';
 
 const AuthorInfo = () => {
   return (  
     <div>
       <p>this is AuthorInfo</p>
+      <DateCard />
     </div>
   )
 }
