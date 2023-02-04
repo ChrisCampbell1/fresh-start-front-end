4bf2bc8958e2f57e1a9bc8708103ff81fbe37e49
Chris Campbell <christopherecampbell@gmail.com>
2023-02-04 11:41:40 -0700

    stub up FeedFilter

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,10 +1,12 @@
 import styles from './Feed.module.css'
-import PostCard from '../PostCard/PostCard';
+import PostCard from '../PostCard/PostCard'
+import FeedFilter from '../FeedFilter/FeedFilter'
 
 const Feed = () => {
   return (  
     <div className={styles.container}>
       <h1>this is a feed</h1>
+      <FeedFilter />
       <PostCard />
       <PostCard />
       <PostCard />
