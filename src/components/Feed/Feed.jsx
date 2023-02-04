3d1b0445fd5baaf2dad9d07da34dd07ac4aea658
Chris Campbell <christopherecampbell@gmail.com>
2023-02-04 10:40:49 -0700

    add PostCard

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,8 +1,14 @@
+import styles from './Feed.module.css'
+import PostCard from '../PostCard/PostCard';
+
 const Feed = () => {
   return (  
-    <>
+    <div>
       <h1>this is a feed</h1>
-    </>
+      <PostCard />
+      <PostCard />
+      <PostCard />
+    </div>
   )
 }
 
---
+++ src/components/PostCard/PostCard.jsx
@@ -0,0 +1,11 @@
+import styles from './PostCard.module.css'
+
+const PostCard = () => {
+  return (  
+  <div>
+    <h2>this is a PostCard</h2>
+  </div>
+  )
+}
+
+export default PostCard;
