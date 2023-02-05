56cdc365bdb30b5c80cb724ef9b4017dcc1e11ca
Chris Campbell <christopherecampbell@gmail.com>
2023-02-05 15:37:03 -0700

    add content and buttons to toggle reviews or subscribers on JourneyDetails

--- src/pages/JourneyDetails/JourneyDetails.jsx
+++ src/pages/JourneyDetails/JourneyDetails.jsx
@@ -2,12 +2,22 @@ import styles from './JourneyDetails.module.css'
 import * as journeyService from '../../services/journeyService'
 import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react'
+import JourneyStats from '../../components/JourneyStats/JourneyStats'
+import RatingCalculator from '../../components/RatingCalculator/RatingCalculator'
+import SubscriberCounter from '../../components/SubscriberCounter/SubscriberCounter'
 
 const JourneyDetails = () => {
   let {id} = useParams()
-  console.log(id, "params")
   
   const [journey, setJourney] = useState({})
+  const [reviewsState, setReviewsState] = useState(true)
+
+  const handleReviewsClick = () => {
+    setReviewsState(true)
+  }
+  const handleSubscribersClick = () => {
+    setReviewsState(false)
+  }
 
   useEffect(() => {
     const fetchJourney = async () => {
@@ -19,7 +29,30 @@ const JourneyDetails = () => {
   
   return (  
     <main className={styles.container}>
-      <h1>This is the JourneyDetails page</h1>
+      <h1>{journey.name}</h1>
+      <img src="https://picsum.photos/600/300" alt="" />
+      <p>{journey.description}</p>
+      <div>
+        <button onClick={() => handleReviewsClick()}>
+          Reviews
+        </button>
+        <button onClick={() => handleSubscribersClick()}>
+          Subscribers
+        </button>
+      </div>
+      {reviewsState ?
+      <div>
+        {journey.reviews.map(review => 
+          <p>this is a review</p>  
+        )}
+      </div>
+      :
+      <div>
+        {journey.subscribers.map(review => 
+          <p>this is a subscriber</p>  
+        )}
+      </div>
+      }
     </main>
   )
 }
