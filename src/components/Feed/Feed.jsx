abeecc0be45047cd9f5f9c0652d3b4c9f354065c
Chris Campbell <christopherecampbell@gmail.com>
2023-02-05 16:48:33 -0700

    connect Feed to backend to fetch posts

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,15 +1,27 @@
 import styles from './Feed.module.css'
+import * as postService from '../../services/postService'
+import { useState, useEffect } from 'react'
 import PostCard from '../PostCard/PostCard'
 import FeedFilter from '../FeedFilter/FeedFilter'
 
 const Feed = () => {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const data = await postService.index()
+      setPosts(data)
+    }
+    fetchPosts()
+  }, [])
+
   return (  
     <div className={styles.container}>
       <h1>this is a feed</h1>
       <FeedFilter />
-      <PostCard />
-      <PostCard />
-      <PostCard />
+      {posts.map(post =>
+        <PostCard key={post._id} post={post} />
+      )}
     </div>
   )
 }
