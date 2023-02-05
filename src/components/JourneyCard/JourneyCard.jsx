738f45f62b690a1f5e0f8bbcf7b64a81d3e6df1d
Chris Campbell <christopherecampbell@gmail.com>
2023-02-05 12:28:38 -0700

    add links to JourneyCard to JourneyDetails with id in params.

--- src/components/JourneyCard/JourneyCard.jsx
+++ src/components/JourneyCard/JourneyCard.jsx
@@ -1,11 +1,16 @@
 import styles from './JourneyCard.module.css'
 import JourneyStats from '../JourneyStats/JourneyStats'
+import { Link } from 'react-router-dom'
 
 const JourneyCard = ({ journey }) => {
   return (  
   <div className={styles.container}>
-    <h2>{journey.name}</h2>
-    <img className={styles.cardPhoto} src="https://picsum.photos/200" alt="journey name" />
+    <Link to={`/journeys/${journey._id}`}>
+      <h2>{journey.name}</h2>
+    </Link>
+    <Link to={`/journeys/${journey._id}`}>
+      <img className={styles.cardPhoto} src="https://picsum.photos/200" alt="journey name" />
+    </Link>
     <p>{journey.description}</p>
     <div className={styles.journeyStats}>
       <JourneyStats journey={journey}/>
