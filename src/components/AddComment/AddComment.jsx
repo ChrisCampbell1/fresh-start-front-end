064d7b511679bb583553007d680f3d819364f519
Chris Campbell <christopherecampbell@gmail.com>
2023-02-06 16:40:28 -0700

    stub up comment components

---
+++ src/components/AddComment/AddComment.jsx
@@ -0,0 +1,9 @@
+import styles from './AddComment.module.css'
+
+const AddComment = ({ post }) => {
+  return (  
+    <p>add comment here</p>
+  )
+}
+
+export default AddComment
---
+++ src/components/CommentsList/CommentsList.jsx
@@ -0,0 +1,9 @@
+import styles from './CommentsList.module.css'
+
+const CommentsList = ({ post }) => {
+  return (  
+    <p>this is a comments list</p>
+  )
+}
+
+export default CommentsList
